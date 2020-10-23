4111c7ef64ad7e08ac6413ad7032820582cd3527
andrebbaccarin <61802808+andrebbaccarin@users.noreply.github.com>
2020-10-23 13:40:05 -0300

    Add files via upload

---
+++ Compile_team1UA_step2.bat
@@ -0,0 +1,8 @@
+@SET PATH=C:/Program Files (x86)/cygwin/bin;%PATH%
+@SET INCLUDE=C:/Program Files (x86)/cygwin/usr/include
+@SET LIB=C:/Program Files (x86)/cygwin/lib
+@SET CYGWIN=nodosfilewarning
+@echo off
+cd .
+call "C:/Program Files (x86)/cygwin/bin/make.exe"
+pause
---
+++ Launch_team1DF.bat
@@ -0,0 +1,7 @@
+@SET PATH=C:/Program Files (x86)/ANSYS Inc/v202/SCADE/SCADE A661/bin;%PATH%
+
+@echo off
+
+cd .
+
+start A661Server.exe "..\DefinitionFile\DF\DF_Generation_server_conf.xml" "..\DefinitionFile\DF\UA_1.bin"
