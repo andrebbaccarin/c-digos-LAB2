49d30a92bf47894c40de20b2a1064288327f5188
andrebbaccarin <61802808+andrebbaccarin@users.noreply.github.com>
2020-10-23 13:33:40 -0300

    Delete Compile_team1UA_step1.bat

--- Compile_team1UA_step1.bat
+++
@@ -1,5 +0,0 @@
-@SET PATH=C:/Program Files (x86)/ANSYS Inc/v202/SCADE/SCADE/bin;%PATH%
-@echo off
-cd .
-call uaadaptor.exe -n "C:\Program Files (x86)\ANSYS Inc\v202\SCADE\SCADE Display\config\a661_description\a661.xml"  -k "..\UserApplication\KCG\kcg_trace.xml" -sdy "..\UserApplication\project1.sdy" -outdir "..\UserApplication\KCG" "..\DefinitionFile\project1.sgfx"
-pause
