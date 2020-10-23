585466c1f58e1a9ad2c0405ee416a51540be332f
andrebbaccarin <61802808+andrebbaccarin@users.noreply.github.com>
2020-10-23 13:33:43 -0300

    Delete Compile_team1UA_step2.bat

--- Compile_team1UA_step2.bat
+++
@@ -1,8 +0,0 @@
-@SET PATH=C:/Program Files (x86)/cygwin/bin;%PATH%
-@SET INCLUDE=C:/Program Files (x86)/cygwin/usr/include
-@SET LIB=C:/Program Files (x86)/cygwin/lib
-@SET CYGWIN=nodosfilewarning
-@echo off
-cd .
-call "C:/Program Files (x86)/cygwin/bin/make.exe"
-pause
